86c7ac122e54836d5b76e0a84af6a6b746c5062d
Ramon Canales <apenasparaspam@gmail.com>
2018-10-07 21:17:25 +0100

    temp fix to reactscrollview

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
@@ -299,6 +299,8 @@ public class ReactScrollView extends ScrollView implements ReactClippingViewGrou
 
   @Override
   public void fling(int velocityY) {
+    velocityY = (int)(Math.abs(velocityY) * Math.signum(mOnScrollDispatchHelper.getYFlingVelocity()));
+
     if (mPagingEnabled) {
       flingAndSnap(velocityY);
     } else if (mScroller != null) {
